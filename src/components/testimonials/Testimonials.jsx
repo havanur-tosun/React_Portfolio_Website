2a0171e20558b08bc0cd885ede599349356dbefb
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-08-13 16:59:45 -0400

    first commit

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./testimonials.scss"
+
+function testimonials() {
+    return (
+        <div className="testimonials">
+            
+        </div>
+    )
+}
+
+export default testimonials;
